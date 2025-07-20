eb66547ef43e8be987ae1218767809e85d2dcbc3
Jiamu Sun <barroit@linux.com>
2025-07-21 01:02:21 +0900

    react: simplify shell component argument list
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- react/shell.jsx
+++ react/shell.jsx
@@ -3,6 +3,7 @@
  * Copyright 2025 Jiamu Sun <barroit@linux.com>
  */
 
+import clsx from 'clsx'
 import { createContext, useRef } from 'react'
 
 export const shell_ctx = createContext(undefined)
@@ -21,24 +22,25 @@ function handle_event(event, shell)
 	}
 }
 
-export default function Shell({ children, color,
-				left, right, padding, offset, move })
+export default function Shell({ children, color, left, right, className })
 {
 	const shell = useRef()
+
+	if (!className) {
+		className = 'before:left-[0.1rem] after:right-[0.1rem] ' +
+			    'hover:before:-translate-x-[0.5rem] ' +
+			    'hover:after:translate-x-[0.5rem] ' +
+			    '*:px-[0.8rem]'
+	}
 
 return (
 <div className='relative inline-block cursor-pointer'
      onClick={ event => handle_event(event, shell) }
      onMouseEnter={ event => handle_event(event, shell) }>
   <ShellContext value={ shell }>
-    <div className='shell'
+    <div className={ clsx('shell', className) }
          data-left={ left ?? '[' } data-right={ right ?? ']' }
-	 style={ {
-		'--shell-color': color ?? '#ff2222',
-		'--shell-padding': padding ?? '0.8rem',
-		'--shell-offset': offset ?? '0.1rem',
-		'--shell-move':move ?? '0.5rem',
-	} }>
+	 style={ { '--shell-color': color ?? '#ff2222' } }>
       { children }
     </div>
   </ShellContext>
