f4dc42da522bee7a197ad678218275dc4a7f3144
Jiamu Sun <barroit@linux.com>
2025-09-27 13:54:09 +0900

    cloudflare: relicense templates
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- worker.js
+++ worker.js
@@ -1,7 +1,4 @@
-/* SPDX-License-Identifier: GPL-3.0-or-later */
-/*
- * Copyright 2025 Jiamu Sun <barroit@linux.com>
- */
+/* SPDX-License-Identifier: CC0-1.0 */
 
 async function fetch_handler(request, env, ctx)
 {
